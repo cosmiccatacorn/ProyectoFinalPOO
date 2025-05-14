1cdea5322fa82879243691618d552dbdd0ac0852
Cata Sandoval <marimeicata@gmail.com>
2025-05-14 18:52:23 -0500

    test

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main (String [ ] args){
+        System.out.println("Este  es un experimento para revisar que mi git está conectado a IntelliJ");
+    }
 }
+
