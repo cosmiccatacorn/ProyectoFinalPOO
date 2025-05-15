3858a2c44340d1220adb556bfb8027a2e1b3f031
Cata Sandoval <marimeicata@gmail.com>
2025-05-15 09:08:22 -0500

    classes created

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String [ ] args){
-        System.out.println("Este  es un experimento para revisar que mi git está conectado a IntelliJ");
+        System.out.println("Este  es un experimento para revisar que mi GitHub está conectado a IntelliJ");
     }
 }
 
