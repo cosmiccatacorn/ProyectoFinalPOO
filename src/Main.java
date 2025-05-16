be2b7f2917a7b039c985c1faa5cbff94e11b4cde
Marshall Gomez <thomasgomez1103@hotmail.com>
2025-05-15 21:39:01 -0500

    Error Arreglado
    Importe las interfaces para que el main funcionara

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import interfaces.ContratoArriendo;
+import interfaces.ContratoCompra;
+import interfaces.ContratoVenta;
+import interfaces.IContrato;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +17,7 @@ public class Main {
         contratos.add(new ContratoVenta(2, 1500000, "Finalizado", "Notaría 7", "2025-02-15"));
         contratos.add(new ContratoArriendo(3, 500000, "En trámite", "Notaría 3", "2025-03-01"));
 
+        System.out.println("\n");
         // Usar polimorfismo para imprimir todos
         for (IContrato contrato : contratos) {
             contrato.crearContrato();
